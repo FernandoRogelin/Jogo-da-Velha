b40a7fbe6b4c7c7c8b617cdd4f9aee5c706e4286
fernando <nando_rogelin@hotmail.com>
2017-12-18 10:57:17 -0200

    [adicionado] tentativa de ver quem ganhou a partida

--- JS/index.js
+++ JS/index.js
@@ -8,22 +8,38 @@ const app = angular.module("tic-tac-toe", [])
 
 
     var letra = 'X';
-    
-    $scope.verifica = function(raiz, raiz2){
-      if($scope.matriz[raiz][raiz2] === 'X' || $scope.matriz[raiz][raiz2] === 'O'){
+
+    $scope.verifica = function (raiz, raiz2) {
+      if ($scope.matriz[raiz][raiz2] === 'X' || $scope.matriz[raiz][raiz2] === 'O') {
         $scope.msg = "Valor inválido!";
-      } else{
+      } else {
         $scope.msg = " ";
         $scope.matriz[raiz][raiz2] = letra;
         var a = (letra === 'X') ? 'O' : 'X';
         letra = a;
+        checa();
       }
     }
-});
 
+    var winner;
 
-/*const app = angular.module('tic-tac-toe', [])
-  .controller('gameController', ['$scope', '$log', function($scope, $log) {
-    $scope.$log = $log;
-    $scope.message = 'Hello World!';
-}]); */
+    function checa() {
+      for (var i = 0; i < 3; i++) {
+        if ($scope.matriz[i][0] === $scope.matriz[i][1] === $scope.matriz[i][2])
+          winner = $scope.matriz[i][0];
+          console.log(winner);
+        if ($scope.matriz[0][i] === $scope.matriz[1][i] === $scope.matriz[2][i])
+          winner = $scope.matriz[0][i];
+      }
+
+      if ($scope.matriz[0][0] === $scope.matriz[1][1] === $scope.matriz[2][2])
+        winner = $scope.matriz[0][0];
+
+      if ($scope.matriz[0][2] === $scope.matriz[1][1] === $scope.matriz[2][0])
+        winner = $scope.matriz[0][2];
+
+      if (winner) {
+        $scope.winner = winner;
+      }
+    }
+  });
